bd6af38d8ddaf6eac0293eba7013eb40c6a503ab
Thuan Hong <thuanhong@FVFWF50XHV2D.local>
2020-06-04 17:56:12 +0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,32 +1,172 @@
-import * as React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import React, {useMemo, useEffect, useContext} from 'react';
+import {
+  NavigationContainer,
+  useNavigation,
+  DrawerActions,
+} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import AppTabNavigation from './components/AppTabNavigation';
 import AuthenticateScreen from './components/AuthenticateScreen';
+import AsyncStorage from '@react-native-community/async-storage';
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+} from '@react-navigation/drawer';
+import {Image, Button} from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+export const AuthContext = React.createContext();
 
 const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const HeaderLeft = () => {
+  const navigation = useNavigation();
+  return (
+    <MaterialCommunityIcons.Button
+      name="menu"
+      size={25}
+      onPress={() => navigation.dispatch(DrawerActions.openDrawer())}
+      backgroundColor="transparent"
+      color="#5F6870"
+    />
+  );
+};
 
 const App = () => {
+  const [state, dispatch] = React.useReducer(
+    (prevState, action) => {
+      switch (action.type) {
+        case 'RESTORE_TOKEN':
+          return {
+            ...prevState,
+            userToken: action.token,
+            isLoading: false,
+          };
+        case 'SIGN_IN':
+          return {
+            ...prevState,
+            isSignout: false,
+            userToken: action.token,
+          };
+        case 'SIGN_OUT':
+          return {
+            ...prevState,
+            isSignout: true,
+            userToken: null,
+          };
+      }
+    },
+    {
+      isLoading: true,
+      isSignout: false,
+      userToken: null,
+    },
+  );
+
+  useEffect(() => {
+    // Fetch the token from storage then navigate to our appropriate place
+    const bootstrapAsync = async () => {
+      let userToken;
+
+      try {
+        userToken = await AsyncStorage.getItem('userToken');
+      } catch (e) {
+        // Restoring token failed
+      }
+
+      // After restoring token, we may need to validate it in production apps
+
+      // This will switch to the App screen or Auth screen and this loading
+      // screen will be unmounted and thrown away.
+      dispatch({type: 'RESTORE_TOKEN', token: userToken});
+    };
+
+    bootstrapAsync();
+  }, []);
+
+  const authContext = useMemo(
+    () => ({
+      signIn: async data => {
+        // In a production app, we need to send some data (usually username, password) to server and get a token
+        // We will also need to handle errors if sign in failed
+        // After getting token, we need to persist the token using `AsyncStorage`
+        // In the example, we'll use a dummy token
+
+        dispatch({type: 'SIGN_IN', token: 'dummy-auth-token'});
+      },
+      signOut: () => dispatch({type: 'SIGN_OUT'}),
+      signUp: async data => {
+        // In a production app, we need to send user data to server and get a token
+        // We will also need to handle errors if sign up failed
+        // After getting token, we need to persist the token using `AsyncStorage`
+        // In the example, we'll use a dummy token
+
+        dispatch({type: 'SIGN_IN', token: 'dummy-auth-token'});
+      },
+    }),
+    [],
+  );
+
   return (
     <NavigationContainer
       style={{
         backgroundColor: '#e8ebee',
       }}>
-      <Stack.Navigator
-        headerMode="screen"
-        initialRouteName="Auth"
-        screenOptions={{
-          headerTintColor: 'black',
-          headerStyle: {backgroundColor: 'white'},
-        }}>
-        <Stack.Screen
-          name="Auth"
-          component={AuthenticateScreen}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen name="Main" component={AppTabNavigation} />
-      </Stack.Navigator>
+      <AuthContext.Provider value={authContext}>
+        <Stack.Navigator
+          headerMode="screen"
+          initialRouteName="Auth"
+          screenOptions={{
+            headerTintColor: 'black',
+            headerStyle: {backgroundColor: 'white'},
+          }}>
+          {state.userToken == null ? (
+            <Stack.Screen
+              name="Auth"
+              component={AuthenticateScreen}
+              options={{
+                headerShown: false,
+                animationTypeForReplace: state.isSignout ? 'pop' : 'push',
+              }}
+            />
+          ) : (
+            <Stack.Screen
+              name="Welcome"
+              component={MainPageDrawerNavigation}
+              options={{
+                headerLeft: () => <HeaderLeft />,
+              }}
+            />
+          )}
+        </Stack.Navigator>
+      </AuthContext.Provider>
     </NavigationContainer>
+  );
+};
+
+const CustomDrawerContent = props => {
+  const {signOut} = useContext(AuthContext);
+  return (
+    <DrawerContentScrollView {...props}>
+      <Image
+        style={{resizeMode: 'stretch', width: '100%', height: 200}}
+        source={require('../assets/drawer.jpg')}
+      />
+      <DrawerItemList {...props} />
+      <Button title="Logout" onPress={signOut} />
+    </DrawerContentScrollView>
+  );
+};
+
+const MainPageDrawerNavigation = () => {
+  return (
+    <Drawer.Navigator
+      drawerContent={props => <CustomDrawerContent {...props} />}>
+      <Drawer.Screen name="Home" component={AppTabNavigation} />
+    </Drawer.Navigator>
   );
 };
 
--- src/components/AuthenticateScreen.js
+++ src/components/AuthenticateScreen.js
@@ -1,21 +1,27 @@
-import React, {useState} from 'react';
+import React, {useState, useContext} from 'react';
 import {
   View,
   KeyboardAvoidingView,
   TextInput,
   StyleSheet,
   TouchableWithoutFeedback,
+  TouchableOpacity,
   Platform,
   Image,
   ImageBackground,
   Dimensions,
+  Text,
 } from 'react-native';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import {AuthContext} from '../App';
 
-const width = Dimensions.get('window').width * 0.7;
+const width = Dimensions.get('window').width;
 
 const AuthenticateScreen = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+
+  const {signIn} = useContext(AuthContext);
 
   return (
     <KeyboardAvoidingView style={styles.container}>
@@ -39,13 +45,37 @@ const AuthenticateScreen = () => {
                   value={username}
                   placeholderTextColor="white"
                 />
+              </View>
+              <View style={styles.inputContainer}>
                 <TextInput
                   placeholder="Password"
                   onChangeText={setPassword}
                   style={styles.input}
                   value={password}
+                  secureTextEntry='true'
                   placeholderTextColor="white"
+                  place
                 />
+              </View>
+            </View>
+            <View style={styles.buttonLogin}>
+              <TouchableOpacity
+                style={styles.navigateBtn}
+                onPress={() => signIn({username, password})}>
+                <Text style={styles.btnTextStyle}>Login</Text>
+              </TouchableOpacity>
+              <View style={styles.btnGroupAuth}>
+                <TouchableOpacity style={styles.btnGroupAuth__btn}>
+                  <AntDesign name="google" size={22} color="white" />
+                </TouchableOpacity>
+                <View style={styles.space} />
+                <TouchableOpacity style={styles.btnGroupAuth__btn}>
+                  <AntDesign name="twitter" size={22} color="white" />
+                </TouchableOpacity>
+                <View style={styles.space} />
+                <TouchableOpacity style={styles.btnGroupAuth__btn}>
+                  <AntDesign name="facebook-square" size={22} color="white" />
+                </TouchableOpacity>
               </View>
             </View>
           </View>
@@ -62,34 +92,66 @@ const styles = StyleSheet.create({
   inner: {
     flex: 1,
     justifyContent: 'space-around',
+    paddingHorizontal: 20,
+    paddingTop: 40,
   },
   imgContainer: {
     flex: 1,
-    alignContent: 'center',
     alignItems: 'center',
+    justifyContent: 'center',
   },
   logo: {
-    flex: 1,
-    width: width,
+    width: width * 0.7,
     resizeMode: 'contain',
   },
   form: {
-    flex: 3,
+    flex: 1,
+    justifyContent: 'center',
   },
   inputContainer: {
     position: 'relative',
   },
   input: {
-    paddingLeft: '10%',
-    margin: 10,
+    paddingVertical: 15,
     borderBottomColor: 'white',
     borderBottomWidth: 1,
     color: 'white',
+    marginVertical: 20,
+    fontSize: 20,
   },
   imgBackground: {
     flex: 1,
     resizeMode: 'cover',
     justifyContent: 'center',
+  },
+  buttonLogin: {
+    flex: 1,
+  },
+  navigateBtn: {
+    backgroundColor: '#F40054',
+    padding: 15,
+    borderRadius: 30,
+  },
+  btnTextStyle: {
+    color: 'white',
+    fontSize: 20,
+    textAlign: 'center',
+  },
+  btnGroupAuth: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 20,
+  },
+  btnGroupAuth__btn: {
+    borderRadius: 30,
+    borderColor: 'white',
+    borderWidth: 1,
+    padding: 12,
+    flex: 3,
+    alignItems: 'center',
+  },
+  space: {
+    flex: 1,
   },
 });
 
